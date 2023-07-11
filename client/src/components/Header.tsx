1f8854ea7f89f871131f1026021ed1c44984e2c6
James <mirageapache@gmail.com>
2023-07-11 22:04:51 +0800

    finish header layout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,6 @@ function Header() {
   return (
     <div id="header">
       <h2 className="header-title">Live Chat</h2>
-
       <button className="btn setting-btn">
         <IconSetting className="setting-icon" />
       </button>
