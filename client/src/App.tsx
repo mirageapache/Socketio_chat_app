df70c886232d6d0f0518db70ae321a021f8c5cb5
James <mirageapache@gmail.com>
2023-07-11 18:15:37 +0800

    Client base css setting & add Header.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import "./styles/css/reset.css";
+import "./styles/css/base.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/css/header.css";
+import { ReactComponent as IconSetting } from "../assets/icons/setting.svg";
+
+function Header() {
+  return (
+    <div id="header">
+      <h2 className="header-title">Live Chat</h2>
+
+      <button className="btn setting-btn">
+        <IconSetting className="setting-icon" />
+      </button>
+    </div>
+  );
+}
+
+export default Header;
