7dc2fea41d0fb0d715aa951ede6a307bce07444c
James <mirageapache@gmail.com>
2023-07-11 15:57:16 +0800

    install client side seckot.io

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+
 const port = process.env.port || 3001;
+const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req,res) => {
-  res.send('express server!');
-})
+const io = new Server(app, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+// 監聽-連線狀態
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
+
+// app.get('/', (req, res) => {
+//   res.send('express server!');
+// });
 
 app.listen(port, () => {
   console.log('Express server running!');
-})
+});
