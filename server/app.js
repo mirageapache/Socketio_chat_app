7f9c251c67936c9890595f2ae62490adf5a464ea
James <mirageapache@gmail.com>
2023-07-12 11:13:33 +0800

    set socket.io connection event

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const port = process.env.port || 3001;
-const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors());
+const server = http.createServer(app);
 
-const io = new Server(app, {
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -16,7 +18,11 @@ const io = new Server(app, {
 
 // 監聽-連線狀態
 io.on('connection', (socket) => {
-  console.log(socket.id);
+  console.log(`User ${socket.id} connected!`);
+
+  socket.on('dissconnect', () => {
+    console.log(`User ${socket.id} discronnected!`);
+  });
 });
 
 // app.get('/', (req, res) => {
