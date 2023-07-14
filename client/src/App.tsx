f9776fca0b064cfb0afdedfbde7226920a110d0e
James <mirageapache@gmail.com>
2023-07-14 19:52:18 +0800

    set react router & predeploy setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,22 @@ import "./styles/css/reset.css";
 import "./styles/css/base.css";
 import { UserProvider } from "contexts/userContext";
 import { ChatProvider } from "contexts/chatContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; //import套件
 
 function App() {
+  const basename = process.env.PUBLIC_URL;
   return (
     <div className="App">
-      <UserProvider>
-        <ChatProvider>
-          <Header />
-          <HomePage />
-        </ChatProvider>
-      </UserProvider>
+      <BrowserRouter basename={basename}>
+        <UserProvider>
+          <ChatProvider>
+            <Header />
+            <Routes>
+              <Route path="*" element={<HomePage />} />
+            </Routes>
+          </ChatProvider>
+        </UserProvider>
+      </BrowserRouter>
     </div>
   );
 }
